4b06289615064f7bca32d2d80d2523e46659e00a
Devika <devikadalal14@gmail.com>
2018-08-30 10:28:05 -0700

    initial commit

---
+++ instagram-api.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+
+    var userFeed = new Instafeed({
+        get: 'user',
+        userId: '6320938276',
+        limit: 12,
+        resolution: 'standard_resolution',
+        accessToken: '6320938276.d90570a.501a6ea3c44c4155bfb0d8172f9aa520',
+        sortBy: 'most-recent',
+        template: '<div class="instagram-post"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="instagram-img"/></a></div>',
+    });
+
+
+    userFeed.run();
+
+    
+    // This will create a single gallery from all elements that have class "gallery-item"
+    $('.gallery').magnificPopup({
+        type: 'image',
+        delegate: 'a',
+        gallery: {
+            enabled: true
+        }
+    });
+
+
+});
